6bd9ce81b607270484a826e478036b7732f2b05e
jae yeong <chujy1224@gmail.com>
2024-03-08 01:21:14 +0900

    feat: 상품 조회 관련 메소드 추가 및 테스트

--- shop-project/src/main/java/com/chujy/shopproject/repository/ItemRepository.java
+++ shop-project/src/main/java/com/chujy/shopproject/repository/ItemRepository.java
@@ -3,6 +3,16 @@ package com.chujy.shopproject.repository;
 import com.chujy.shopproject.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemName(String itemName);
+
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
 }
--- shop-project/src/test/java/com/chujy/shopproject/repository/ItemRepositoryTest.java
+++ shop-project/src/test/java/com/chujy/shopproject/repository/ItemRepositoryTest.java
@@ -2,17 +2,24 @@ package com.chujy.shopproject.repository;
 
 import com.chujy.shopproject.constant.ItemSellStatus;
 import com.chujy.shopproject.domain.Item;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.yml")
+@Transactional
+@Rollback
 class ItemRepositoryTest {
+
+    @Autowired
+    EntityManager em;
 
     @Autowired
     ItemRepository itemRepository;
@@ -35,6 +42,67 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
 
         System.out.println(savedItem.toString());
+
+    }
+
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+
+            item.setItemName("테스트상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNameTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemName("테스트상품1");
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNameOrItemDetail() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNameOrItemDetail("테스트상품1", "테스트 상품 상세 설명5");
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThen 테스트")
+    public void findByPriceLessThan() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 
 }

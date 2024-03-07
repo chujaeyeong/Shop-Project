a40b2b69202e7d503429009cd33982651fab8b61
jae yeong <chujy1224@gmail.com>
2024-03-07 17:18:00 +0900

    build: 프로젝트 세팅 파일 추가

---
+++ shop-project/src/main/java/com/chujy/shopproject/ShopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chujy.shopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProjectApplication.class, args);
+	}
+
+}
---
+++ shop-project/src/test/java/com/chujy/shopproject/ShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chujy.shopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

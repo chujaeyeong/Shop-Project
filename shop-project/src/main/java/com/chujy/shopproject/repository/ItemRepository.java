7be90cbe7add4f92ee9551e8bde606aaafdf2d2b
jae yeong <chujy1224@gmail.com>
2024-03-09 00:19:48 +0900

    feat: 상품 리포지토리 계층에 QueryDslPredicateExcutor 인터페이스 상속 추가 및 상품 조회 테스트

--- shop-project/src/main/java/com/chujy/shopproject/repository/ItemRepository.java
+++ shop-project/src/main/java/com/chujy/shopproject/repository/ItemRepository.java
@@ -3,11 +3,12 @@ package com.chujy.shopproject.repository;
 import com.chujy.shopproject.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemName(String itemName);
 
--- shop-project/src/test/java/com/chujy/shopproject/repository/ItemRepositoryTest.java
+++ shop-project/src/test/java/com/chujy/shopproject/repository/ItemRepositoryTest.java
@@ -3,6 +3,7 @@ package com.chujy.shopproject.repository;
 import com.chujy.shopproject.constant.ItemSellStatus;
 import com.chujy.shopproject.domain.Item;
 import com.chujy.shopproject.domain.QItem;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -10,8 +11,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -48,6 +53,7 @@ class ItemRepositoryTest {
 
     }
 
+    // 상품 데이터 1
     public void createItemList() {
         for (int i = 1; i <= 10; i++) {
             Item item = new Item();
@@ -146,6 +152,66 @@ class ItemRepositoryTest {
 
         for (Item item : itemList) {
             System.out.println(item.toString());
+        }
+    }
+
+    // 상품 데이터 2
+    public void createItemList2() {
+        for (int i = 1; i <= 5; i++) {
+            Item item = new Item();
+
+            item.setItemName("테스트상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            itemRepository.save(item);
+        }
+
+        for (int i = 6; i <= 10; i++) {
+            Item item = new Item();
+
+            item.setItemName("테스트상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("QueryDSL 조회 테스트2")
+    public void queryDSLTest2() {
+        this.createItemList2();
+        BooleanBuilder booleanBuilder = new BooleanBuilder(); // 쿼리에 들어갈 조건을 만들어주는 빌더
+        QItem item = QItem.item;
+
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStatus = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        if (StringUtils.equals(itemSellStatus, ItemSellStatus.SELL)) {
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : " + itemPagingResult.getTotalElements());
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+
+        for (Item resultItem : resultItemList) {
+            System.out.println(resultItem.toString());
         }
     }
 

970031c4bb0fb8cc55ac0807f63a30429921df76
jae yeong <chujy1224@gmail.com>
2024-03-10 22:59:25 +0900

    build: 타임리프 페이지 레이아웃 설정 추가, 타임리프 기능 테스트 진행 완료

---
+++ shop-project/src/main/java/com/chujy/shopproject/dto/ItemDto.java
@@ -0,0 +1,24 @@
+package com.chujy.shopproject.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+
+    private Long id;
+
+    private String itemName;
+
+    private Integer price;
+
+    private String itemDetail;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+}

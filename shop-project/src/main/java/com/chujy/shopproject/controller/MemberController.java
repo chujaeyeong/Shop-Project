497a4bda0f409e3cb02e14b92acb2b0ff59161ae
jae yeong <chujy1224@gmail.com>
2024-03-11 16:05:14 +0900

    feat: 회원가입 검증 로직 구현, html 오탈자 수정 (main, memberForm)

--- shop-project/src/main/java/com/chujy/shopproject/controller/MemberController.java
+++ shop-project/src/main/java/com/chujy/shopproject/controller/MemberController.java
@@ -3,10 +3,12 @@ package com.chujy.shopproject.controller;
 import com.chujy.shopproject.domain.Member;
 import com.chujy.shopproject.dto.MemberFormDto;
 import com.chujy.shopproject.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +28,24 @@ public class MemberController {
     }
 
     @PostMapping("/new")
-    public String memberForm(MemberFormDto memberFormDto) {
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+
+        // 에러가 있다면 회원가입 페이지로 이동
+        if (bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        // 에러가 없으면 회원 저장
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        }
+
+        // 회원가입 시 중복 회원 가입 에러 발생하면 에러 메시지를 뷰로 전달
+        catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
--- shop-project/src/main/java/com/chujy/shopproject/dto/MemberFormDto.java
+++ shop-project/src/main/java/com/chujy/shopproject/dto/MemberFormDto.java
@@ -1,18 +1,26 @@
 package com.chujy.shopproject.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 public class MemberFormDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
     private String email;
 
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
 
 }

51d396940d4a1e9db131206be545b34a49971419
jae yeong <chujy1224@gmail.com>
2024-05-12 18:17:18 +0900

    refactor: SecurityConfig import 코드 정리

--- shop-project/src/main/java/com/chujy/shopproject/config/SecurityConfig.java
+++ shop-project/src/main/java/com/chujy/shopproject/config/SecurityConfig.java
@@ -2,20 +2,14 @@ package com.chujy.shopproject.config;
 
 import com.chujy.shopproject.oauth.service.CustomOAuth2UserService;
 import com.chujy.shopproject.service.MemberService;
-import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
